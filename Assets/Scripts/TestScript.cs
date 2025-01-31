f7a0b647043d9e9b937b895d5de4a6f8648adb49
Kla <naphat.watt@bumail.net>
2025-01-31 07:46:47 +0700

    SAVE1

---
+++ Assets/Scripts/TestScript.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    [SerializeField] private InputReader inputReader;
+    // Start is called before the first frame update
+    void Start()
+    {
+        inputReader.MoveEvent += HandleMove;
+    }
+    
+    private void OnDestroy()
+    {
+        inputReader.MoveEvent -= HandleMove;
+    }
+    
+    private void HandleMove(Vector2 movement)
+    {
+        Debug.Log(movement);
+    }
+}
